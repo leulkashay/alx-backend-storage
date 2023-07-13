29ff4f2f6b884bd0e97e9d2be905a0c4bad2cce0
leulkashay <leulkashay2012@gmail.com>
2023-07-13 09:04:31 +0000

    update

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,3 @@
+-- script that creates an index idx_name_first on the table names and the first letter of name
+-- only the first letter of name must be index
+CREATE INDEX idx_name_first ON names(name(1))
---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,2 @@
+-- script INDEX idx_name_first_score on the table names
+CREATE idx_name_first_score on names(name(1), score)
